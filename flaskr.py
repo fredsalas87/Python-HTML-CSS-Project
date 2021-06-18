176ddecfa27f777d3ec6c9c4c0819b45af10ce56
Fredy Rolando Salazar Sangucho <fredsalas87@gmail.com>
2021-06-18 17:01:01 +0000

    Add more files

---
+++ flaskr.py
@@ -0,0 +1,17 @@
+import sqlite3
+from flask import Flask
+
+# Configurações
+
+DATABASE = './flaskr.db'
+SECRET_KEY = "app"
+USERNAME = 'fredy'
+PASWORD = '120609'
+
+# Aplicação
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
